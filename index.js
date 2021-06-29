51d1a3c21ed67031cdd2fdbdf93f472095320cfb
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-06-29 17:20:50 -0500

    Servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('desde index');
+//importar express
+const express = require('express');
+
+//Crear el servidor
+const app = express();
+
+//Si hay puerto en variable de entorno tomar ese si no el puerto 4000
+const PORT = process.env.PORT || 4000;
+
+//Arrancar el servidor
+app.listen(PORT, () =>{
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
