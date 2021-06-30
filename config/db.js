96466743d9b2cae46479a1da5f256389d3def3a3
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-06-29 22:02:44 -0500

    Conectar DB de mongo con Variable de entorno

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config({path: 'variables.env'}); //Se requiere dotenv y le pasamos como configuracion que donde va encontrar esas variables va ser en el archivo variables.env
+
+const conectarDB = async () =>{
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            //config para ara algunos errores
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('DB conectada')
+    } catch (error) {
+        console.log(error);
+        process.exit(1); //Detener la app
+    }
+}
+
+module.exports = conectarDB; //Para poder usar la funcion en mas componentes
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //importar express
 const express = require('express');
+//Importar conectarDB
+const conectarDB = require('./config/db');
 
 //Crear el servidor
 const app = express();
+
+//Conectar a la db
+conectarDB();
 
 //Si hay puerto en variable de entorno tomar ese si no el puerto 4000
 const PORT = process.env.PORT || 4000;
