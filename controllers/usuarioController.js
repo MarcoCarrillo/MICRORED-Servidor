39661d22a45e3bedaa2b6cef972adee9f60e4402
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-06-30 21:40:48 -0500

    Insertando registros desde postman en BDD
    Insertando registros de usuariosEmpresa y usuarios desde postman

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,21 @@
 //Funciones llamadas entre el proceso de la peticion y el envio de la respuesta (Middleware)
 //Exports para poder usarla en otros archivos y como parametro se le pasa el request y la response (peticion y respuesta)
-exports.crearUsuario = (req, res) =>{
-    console.log(req.body)
+const Usuario = require('../models/Usuario')
+
+exports.crearUsuario = async (req, res) =>{
+    try {
+        let usuario;
+
+        //crea el nuevo usuario que va tener los campos del modelo
+        usuario = new Usuario(req.body);
+
+        //guardar el usuario
+        await usuario.save();
+
+        //Mensaje de confirmacion
+        res.send('Usuario creado correctamente');
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Hubo un error'); //Status 400 es una bad request o una peticion mal
+    }
 }
--- controllers/usuarioEmpresaController.js
+++ controllers/usuarioEmpresaController.js
@@ -1,4 +1,14 @@
+const UsuarioEmpresa = require('../models/UsuarioEmpresa');
 
-exports.crearUsuarioEmpresa = (req, res) =>{
-    console.log(req.body)
+exports.crearUsuarioEmpresa = async (req, res) =>{
+    try {
+        let usuarioEmpresa;
+        usuarioEmpresa = new UsuarioEmpresa(req.body);
+        await usuarioEmpresa.save();
+        res.send('Usuario privado creado correctamente');
+        console.log(req.body);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error en el registro');
+    }
 }
